991bd9bf7a01f7f095f4885700a006050b7186d7
hkrist <chris.hvse@gmail.com>
2018-05-05 21:31:37 +0200

    code ordering fixed

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -67,36 +67,30 @@ public class GT4500 implements SpaceShip {
         if (! secondaryTorpedoStore.isEmpty()) {
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
-        }
-        else {
+        } else {
           // although primary was fired last time, but the secondary is empty
           // thus try to fire primary again
           if (! primaryTorpedoStore.isEmpty()) {
             firingSuccess = primaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = true;
           }
-
-          // if both of the stores are empty, nothing can be done, return failure
         }
-      }
-      else {
+      } else {
         // try to fire the primary first
         if (! primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = true;
-        }
-        else {
+        } else {
           // although secondary was fired last time, but primary is empty
           // thus try to fire secondary again
           if (! secondaryTorpedoStore.isEmpty()) {
             firingSuccess = secondaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = false;
           }
-
           // if both of the stores are empty, nothing can be done, return failure
         }
       }
-     } else if (firingMode.equals(FiringMode.ALL)) {
+    } else if (firingMode.equals(FiringMode.ALL)) {
       // try to fire both of the torpedo stores
       if ((! primaryTorpedoStore.isEmpty()) && (! secondaryTorpedoStore.isEmpty())) {
         firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
